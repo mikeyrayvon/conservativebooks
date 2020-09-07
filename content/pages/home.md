ff545cd17bc9ced0f1f723fdb708b940746d5f5f
mikeyrayvon <mikey@interglobal.vision>
2020-09-07 13:48:36 +0200

    remove siteconfig. move description field to home page

--- content/pages/home.md
+++ content/pages/home.md
@@ -1,4 +1,5 @@
 ---
 title: Conservative Books
+description: Conservative Books is an experimental publisher with 58 years of experience.
 ---
 Conservative Books is an experimental publisher with 58 years of experience. Our catalogue includes materials on topics such as sex education, philosophy, fire trucks, and secrets. Conservative Books promotes the conservation of conversation and regularly publishes with leading art institutions.
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,20 @@ import Head from 'next/head'
 import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter'
 
-const Home = ({ siteTitle, siteDescription, frontmatter, markdownBody }) => {
+const Home = ({ frontmatter, markdownBody }) => {
   if (!frontmatter) return <></>
   const {
-    title
+    title,
+    description
   } = JSON.parse(frontmatter)
   return (
     <>
       <Head>
-        <title>{siteTitle}</title>
-        <meta name="description" content={siteDescription} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <meta property="og:url" content='' key='url' />
-        <meta property="og:title" content={siteTitle} key='title' />
-        <meta property="og:description" content={siteDescription} key='description' />
+        <meta property="og:title" content={title} key='title' />
+        <meta property="og:description" content={description} key='description' />
       </Head>
       <div className='container'>
         <h1>{title}</h1>
@@ -29,13 +30,10 @@ export default Home
 
 export async function getStaticProps() {
   const content = await import(`../content/pages/home.md`)
-  const config = await import(`../siteconfig.json`)
   const data = matter(content.default)
 
   return {
     props: {
-      siteTitle: config.title,
-      siteDescription: config.description,
       frontmatter: JSON.stringify(data.data),
       markdownBody: data.content,
     }
